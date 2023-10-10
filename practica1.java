c2aafac53db166d279af6d102de6ab724f6f6300
Dani <dsegura20219@iesjoanramis.org>
2023-10-10 19:55:11 +0200

    Modificacio amb comentaris

--- practica1.java
+++ practica1.java
@@ -1,6 +1,9 @@
+/* fer un programa que mostri
+per pantalla "hola mundo */
 public class practica1 {
 
 	public static void main(String[] args) {
+//Aquesta part del programa escriu hola mundo!!
 		System.out.println("Hola Mundo!!");
 
 	}
